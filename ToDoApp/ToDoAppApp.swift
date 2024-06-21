2fe80e5e6b63fb17459e66fa41b6709857c18dde
vtagilov <v.tagilov@mail.ru>
2024-06-21 10:58:18 +0300

    Create SwiftUI app

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Владимир on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
