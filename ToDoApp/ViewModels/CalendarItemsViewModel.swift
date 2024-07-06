035934fdf43d279756487fc4152a4b8a4e0d1ac7
vtagilov <v.tagilov@mail.ru>
2024-07-06 03:06:26 +0300

    Add main ViewController & ViewModel

---
+++ ToDoApp/ViewModels/CalendarItemsViewModel.swift
@@ -0,0 +1,110 @@
+//
+//  CalendarItemsViewModel.swift
+//  ToDoApp
+//
+//  Created by Владимир on 04.07.2024.
+//
+
+import Foundation
+
+final class CalendarItemsViewModel {
+    var selectedItem: TodoItem?
+    private(set) var items: [TodoItem]
+    private(set) var itemsDict = [String: [TodoItem]]() // deadline: [item]
+    
+    private let cache = FileCache()
+    private let defaultFileName = "TodoItemList"
+    
+    private(set) lazy var uniqueDeadlines = [String]()
+    
+    init() {
+        cache.loadItemsFromFile(defaultFileName)
+        items = cache.items
+        reloadDict()
+    }
+    
+    func addItem(_ item: TodoItem) {
+        if items.contains(where: { $0.id == item.id }) {
+            updateItem(item)
+        } else {
+            items.append(item)
+        }
+        cache.addItem(item)
+        cache.saveItemsToFile(defaultFileName)
+        reloadDict()
+    }
+    
+    func updateItem(_ item: TodoItem, _ isDone: Bool) {
+        if let index = items.firstIndex(where: { $0.id == item.id }) {
+            let oldItem = items.remove(at: index)
+            let newItem = TodoItem(
+                id: oldItem.id,
+                text: oldItem.text,
+                importance: oldItem.importance,
+                isDone: isDone,
+                creationDate: oldItem.creationDate,
+                deadline: oldItem.deadline,
+                editedDate: oldItem.editedDate
+            )
+            items.insert(newItem, at: index)
+            reloadDict()
+            cache.updateItem(newItem)
+            cache.saveItemsToFile(defaultFileName)
+        }
+    }
+    
+    
+    
+    func removeItem(_ item: TodoItem) {
+        if let index = items.firstIndex(where: { $0.id == item.id }) {
+            items.remove(at: index)
+        }
+        cache.removeItem(item.id)
+        cache.saveItemsToFile(defaultFileName)
+        reloadDict()
+    }
+    
+    func getItemById(itemId: String) -> TodoItem? {
+        return items.first(where: { $0.id == itemId })
+    }
+    
+    func selectItemById(itemId: String) {
+        selectedItem = items.first(where: { $0.id == itemId })
+    }
+    
+    private func updateItem(_ newItem: TodoItem) {
+        if let index = items.firstIndex(where: { $0.id == newItem.id }) {
+            items[index] = newItem
+        }
+        cache.updateItem(newItem)
+    }
+    
+    private func reloadDict() {
+        itemsDict = [:]
+        uniqueDeadlines = []
+        var uniqueDeadlines = [Int]()
+        for item in items {
+            if let deadline = item.deadline {
+                let date = DateFormatter.convertToStringDayMonth(deadline)
+                if itemsDict[date] == nil {
+                    itemsDict[date] = []
+                    uniqueDeadlines.append(Int(deadline.timeIntervalSince1970))
+                }
+                itemsDict[date]?.append(item)
+            } else {
+                if itemsDict["Другое"] == nil {
+                    itemsDict["Другое"] = []
+                    uniqueDeadlines.append(Int.max)
+                }
+                itemsDict["Другое"]?.append(item)
+            }
+        }
+        uniqueDeadlines = uniqueDeadlines.sorted()
+        self.uniqueDeadlines = uniqueDeadlines.map({
+            if $0 == Int.max {
+                return "Другое"
+            }
+            return DateFormatter.convertToStringDayMonth(.init(timeIntervalSince1970: TimeInterval($0)))
+        })
+    }
+}
---
+++ ToDoApp/Views/Calendar/CalendarViewController.swift
@@ -0,0 +1,130 @@
+//
+//  CalendarViewController.swift
+//  ToDoApp
+//
+//  Created by Владимир on 04.07.2024.
+//
+
+import UIKit
+import SwiftUI
+
+struct CalendarViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UINavigationController {
+        let calendarVC = CalendarViewController()
+        let navigationController = UINavigationController(rootViewController: calendarVC)
+        return navigationController
+    }
+    
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+    }
+}
+
+
+final class CalendarViewController: UIViewController {
+    private let viewModel = CalendarItemsViewModel()
+    
+    private var itemsView: CalendarItemsView!
+    private var datesView: CalendarDatesView!
+    
+    private let plusButton: UIButton = {
+        let plusImage = UIImage(systemName: "plus.circle.fill")?.withTintColor(UIColor(Color.Palette.White.color))
+        let button = UIButton()
+        button.backgroundColor = UIColor(Color.Palette.White.color)
+        button.setImage(plusImage, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFill
+        button.contentHorizontalAlignment = .fill
+        button.contentVerticalAlignment = .fill
+        return button
+    }()
+    
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(Color.Palette.Gray.color).withAlphaComponent(0.3)
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpUI()
+        setNavigationBar()
+        setConstraints()
+    }
+    
+    private func setUpUI() {
+        self.view.backgroundColor = UIColor(Color.Back.Primary.color)
+        
+        datesView = CalendarDatesView(viewModel: viewModel, itemsView: nil)
+        itemsView = CalendarItemsView(viewModel: viewModel, datesView: datesView, openDetailScreen: openDetailScreen)
+        datesView.itemsView = itemsView
+        
+        plusButton.addTarget(self, action: #selector(openDetailScreen), for: .touchUpInside)
+    }
+    
+    private func setNavigationBar() {
+        self.title = "Мои дела"
+        let closeButton = UIBarButtonItem(
+            image: UIImage(systemName: "list.bullet"),
+            style: .plain,
+            target: self,
+            action: #selector(closeViewController)
+        )
+        self.navigationItem.leftBarButtonItem = closeButton
+    }
+
+    @objc private func closeViewController() {
+        self.dismiss(animated: true)
+    }
+    
+    @objc private func openDetailScreen() {
+        let detailView = DetailScreen(
+            item: viewModel.selectedItem,
+            saveAction: { item in
+                self.viewModel.addItem(item)
+                self.itemsView.reloadData()
+                self.datesView.reloadData()
+            },
+            deleteAction: { item in
+                self.viewModel.removeItem(item)
+                self.itemsView.reloadData()
+                self.datesView.reloadData()
+            }
+        )
+        let hostingController = UIHostingController(rootView: detailView)
+        present(hostingController, animated: true, completion: {
+            self.viewModel.selectedItem = nil
+        })
+    }
+}
+
+extension CalendarViewController {
+    private func setConstraints() {
+        for subview in [datesView, itemsView, plusButton, separatorView] {
+            if let subview = subview {
+                view.addSubview(subview)
+                subview.translatesAutoresizingMaskIntoConstraints = false
+            }
+        }
+        
+        NSLayoutConstraint.activate([
+            datesView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            datesView.heightAnchor.constraint(equalToConstant: 75),
+            datesView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            datesView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            separatorView.heightAnchor.constraint(equalToConstant: 1),
+            separatorView.topAnchor.constraint(equalTo: datesView.bottomAnchor),
+            separatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            separatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            itemsView.topAnchor.constraint(equalTo: separatorView.bottomAnchor),
+            itemsView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            itemsView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            itemsView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            plusButton.heightAnchor.constraint(equalToConstant: 44),
+            plusButton.widthAnchor.constraint(equalToConstant: 44),
+            plusButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            plusButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
